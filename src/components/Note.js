590b789dc423b9fccaecbbeee4e928ed1ed589c8
unknown <myesetnod3223@gmail.com>
2023-07-16 22:22:15 +0500

    Initial commit

---
+++ src/components/Note.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+const Note = () => {
+  return (
+    <div className="note">
+      <div>
+        <h1>Title</h1>
+        <p>Info</p>
+      </div>
+    </div>
+  );
+};
+
+export default Note;
